c24fdaba047bcdbb4d5a54f3e0df69d8357433d4
kakashilfc <118949513+kakashilfc@users.noreply.github.com>
2023-01-06 07:25:17 +0000

    2

--- next.config.js
+++ next.config.js
@@ -16,4 +16,31 @@ module.exports = {
       'secure.gravatar.com',
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/teamkkk',
+        destination: '/about11',
+        permanent: false,
+      },
+      // Path Matching - will match `/old-blog/a`, but not `/old-blog/a/b`
+      /*{
+        source: '/old-blog/:slug',
+        destination: '/news/:slug',
+        permanent: false,
+      },*/
+      // Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
+      {
+        source: '/blog/:slug*',
+        destination: '/news/:slug*',
+        permanent: false,
+      },
+      // Regex Path Matching - The regex below will match `/post/123` but not `/post/abc`
+      {
+        source: '/post/:slug(\\d{1,})',
+        destination: '/news/:slug',
+        permanent: false,
+      },
+    ]
+  },
 }
